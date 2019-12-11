6ad6c44a8bf41685a5a1ab509eb327b7419d6aa2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-11 16:47:04 +0900

    fix default value

--- db/migrate/20191211073527_add_default_quantity_to_cart_item.rb
+++ db/migrate/20191211073527_add_default_quantity_to_cart_item.rb
@@ -1,5 +1,5 @@
 class AddDefaultQuantityToCartItem < ActiveRecord::Migration[5.2]
   def change
-    change_column_default :cart_item, :quantity, 0
+    change_column_default :cart_items, :quantity, default: 0, null: false
   end
 end
