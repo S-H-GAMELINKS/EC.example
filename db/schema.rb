4cab390f7adcd9dda659dc978870f01b6daaa25f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-11 11:39:00 +0900

    Migrate DB for ActiveStorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_022820) do
+ActiveRecord::Schema.define(version: 2019_12_11_022957) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_12_11_022820) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "images"
   end
 
   create_table "users", force: :cascade do |t|
