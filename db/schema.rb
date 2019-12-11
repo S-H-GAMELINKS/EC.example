b6de1df2fc80717be4aa0def7c6b3e18cee3c200
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-11 16:47:09 +0900

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_070129) do
+ActiveRecord::Schema.define(version: 2019_12_11_073527) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
