517682aae0bc37a1b0d4a8321c6eaaa19aeca91e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-16 17:19:59 +0900

    impl search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
+
+  PER = 9
+
   def search
+    @products = Product.ransack(title_or_description_cont: params[:keyword]).result.page(params[:page]).per(PER)
   end
 end
